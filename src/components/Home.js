fa4dce81b41606ddcac7cad00aaeeaa876695dcb
kenwaysharma <kenwaysharma@gmail.com>
2021-10-03 23:33:03 +0530

    Release Build2

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import {
     BrowserRouter as Router,
     Switch,
     Route,
+    Redirect,
     NavLink
 } from "react-router-dom";
 import Chart from "./Chart";
@@ -13,21 +14,32 @@ export default function Home() {
     return (
         <Router>
             <div className="mainDiv">
-                <div className="details">
+                <header className="details">
                     <h1>Made by Varun Sharma</h1>
                     <h3>8802683829 | kenwaysharma@gmail.com</h3>
 
 
-                </div>
+                </header>
 
                 <nav className="ContentNav">
+
                     <NavLink to="/table" activeClassName="active">Table</NavLink>
 
                     <NavLink to="/chart">Chart</NavLink>
                 </nav>
+
+
             </div>
-            <Route path="/table"><Table /></Route>
-            <Route path="/chart"><Chart /></Route>
+
+            <Switch>
+                <Redirect exact from="/" to="/table" component={Table} />
+                <Route path="/table"><Table /></Route>
+                <Route path="/chart"><Chart /></Route>
+            </Switch>
+
+            <footer>
+                <p>Made using<a href="https://reactjs.org/"> Reactjs </a>and<a href="https://www.chartjs.org/"> Chartjs </a></p>
+            </footer>
         </Router>
     );
 
